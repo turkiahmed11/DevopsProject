46ae5fe4b097c5c3910e6492f88ce05c6dd3ea28
unknown <malek.ksontini@esprit.tn>
2024-11-10 20:06:50 +0100

    Journalisation

--- src/main/java/tn/esprit/spring/kaddem/services/EquipeServiceImpl.java
+++ src/main/java/tn/esprit/spring/kaddem/services/EquipeServiceImpl.java
@@ -2,7 +2,6 @@ package tn.esprit.spring.kaddem.services;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import lombok.extern.slf4j.Slf4j;
 import tn.esprit.spring.kaddem.entities.Contrat;
 import tn.esprit.spring.kaddem.entities.Equipe;
@@ -17,66 +16,78 @@ import java.util.Set;
 @Slf4j
 @AllArgsConstructor
 @Service
-public class EquipeServiceImpl implements IEquipeService{
-	EquipeRepository equipeRepository;
+public class EquipeServiceImpl implements IEquipeService {
 
+	private final EquipeRepository equipeRepository;
 
-	public List<Equipe> retrieveAllEquipes(){
-	return  (List<Equipe>) equipeRepository.findAll();
-	}
-	public Equipe addEquipe(Equipe e){
-		return (equipeRepository.save(e));
+	public List<Equipe> retrieveAllEquipes() {
+		log.info("Retrieving all teams");
+		return (List<Equipe>) equipeRepository.findAll();
 	}
 
-	public  void deleteEquipe(Integer idEquipe){
-		Equipe e=retrieveEquipe(idEquipe);
-		equipeRepository.delete(e);
+	public Equipe addEquipe(Equipe e) {
+		log.info("Adding a new team: {}", e);
+		return equipeRepository.save(e);
 	}
 
-	public Equipe retrieveEquipe(Integer equipeId){
-		return equipeRepository.findById(equipeId).get();
+	public void deleteEquipe(Integer idEquipe) {
+		Equipe e = retrieveEquipe(idEquipe);
+		if (e != null) {
+			log.info("Deleting team with ID: {}", idEquipe);
+			equipeRepository.delete(e);
+		} else {
+			log.warn("Cannot delete team. Team with ID {} not found", idEquipe);
+		}
 	}
 
-	public Equipe updateEquipe(Equipe e){
-	return (	equipeRepository.save(e));
+	public Equipe retrieveEquipe(Integer equipeId) {
+		log.info("Retrieving team with ID: {}", equipeId);
+		return equipeRepository.findById(equipeId).orElse(null);
 	}
 
-	public void evoluerEquipes(){
+	public Equipe updateEquipe(Equipe e) {
+		log.info("Updating team: {}", e);
+		return equipeRepository.save(e);
+	}
+
+	public void evoluerEquipes() {
 		List<Equipe> equipes = (List<Equipe>) equipeRepository.findAll();
+
 		for (Equipe equipe : equipes) {
-			if ((equipe.getNiveau().equals(Niveau.JUNIOR)) || (equipe.getNiveau().equals(Niveau.SENIOR))) {
-				List<Etudiant> etudiants = (List<Etudiant>) equipe.getEtudiants();
-				Integer nbEtudiantsAvecContratsActifs=0;
-				for (Etudiant etudiant : etudiants) {
-					Set<Contrat> contrats = etudiant.getContrats();
-					//Set<Contrat> contratsActifs=null;
-					for (Contrat contrat : contrats) {
+			// Vérifier si l'équipe est éligible pour une évolution
+			if (equipe.getNiveau() == Niveau.JUNIOR || equipe.getNiveau() == Niveau.SENIOR) {
+
+				int nbEtudiantsAvecContratsActifs = 0;
+
+				for (Etudiant etudiant : equipe.getEtudiants()) {
+					for (Contrat contrat : etudiant.getContrats()) {
 						Date dateSysteme = new Date();
-						long difference_In_Time = dateSysteme.getTime() - contrat.getDateFinContrat().getTime();
-						long difference_In_Years = (difference_In_Time / (1000l * 60 * 60 * 24 * 365));
-						if ((contrat.getArchive() == false) && (difference_In_Years > 1)) {
-							//	contratsActifs.add(contrat);
+						long differenceInTime = dateSysteme.getTime() - contrat.getDateFinContrat().getTime();
+						long differenceInYears = differenceInTime / (1000L * 60 * 60 * 24 * 365);
+
+						// Vérifier si le contrat est actif depuis plus d'un an
+						if (!contrat.getArchive() && differenceInYears > 1) {
 							nbEtudiantsAvecContratsActifs++;
-							break;
+							break; // Un étudiant avec un contrat actif trouvé, passer au suivant
 						}
-						if (nbEtudiantsAvecContratsActifs >= 3) break;
+					}
+
+					if (nbEtudiantsAvecContratsActifs >= 3) {
+						break; // Critère atteint, on peut arrêter la recherche
 					}
 				}
-					if (nbEtudiantsAvecContratsActifs >= 3){
-						if (equipe.getNiveau().equals(Niveau.JUNIOR)){
-							equipe.setNiveau(Niveau.SENIOR);
-							equipeRepository.save(equipe);
-							break;
-						}
-						if (equipe.getNiveau().equals(Niveau.SENIOR)){
-							equipe.setNiveau(Niveau.EXPERT);
-							equipeRepository.save(equipe);
-							break;
-						}
+
+				// Évolution de l'équipe si elle remplit les critères
+				if (nbEtudiantsAvecContratsActifs >= 3) {
+					if (equipe.getNiveau() == Niveau.JUNIOR) {
+						equipe.setNiveau(Niveau.SENIOR);
+					} else if (equipe.getNiveau() == Niveau.SENIOR) {
+						equipe.setNiveau(Niveau.EXPERT);
+					}
+					equipeRepository.save(equipe); // Sauvegarder les changements
 				}
 			}
-
 		}
-
 	}
+
 }
