2d8769ce3f7ad11b4c033cbb70d612dabcfee589
medsahbi10 <medsahbi.benrejeb@gmail.com>
2024-11-10 02:32:26 +0100

    Update UniversiteServiceImpl.java

--- src/main/java/tn/esprit/spring/kaddem/services/UniversiteServiceImpl.java
+++ src/main/java/tn/esprit/spring/kaddem/services/UniversiteServiceImpl.java
@@ -6,6 +6,9 @@ import tn.esprit.spring.kaddem.entities.Departement;
 import tn.esprit.spring.kaddem.entities.Universite;
 import tn.esprit.spring.kaddem.repositories.DepartementRepository;
 import tn.esprit.spring.kaddem.repositories.UniversiteRepository;
+import java.util.Optional; 
+import javax.persistence.EntityNotFoundException; 
+
 
 import java.util.List;
 import java.util.Set;
@@ -16,8 +19,8 @@ public class UniversiteServiceImpl implements IUniversiteService{
     UniversiteRepository universiteRepository;
 @Autowired
     DepartementRepository departementRepository;
-    public UniversiteServiceImpl() {
-        // TODO Auto-generated constructor stub
+    public UniversiteServiceImpl(UniversiteRepository universiteRepository) {
+        this.universiteRepository = universiteRepository;
     }
   public   List<Universite> retrieveAllUniversites(){
 return (List<Universite>) universiteRepository.findAll();
@@ -30,24 +33,43 @@ return  (universiteRepository.save(u));
  public    Universite updateUniversite (Universite  u){
      return  (universiteRepository.save(u));
     }
+public Universite retrieveUniversite(Integer idUniversite) {
+        Optional<Universite> optionalUniversite = universiteRepository.findById(idUniversite);
 
-  public Universite retrieveUniversite (Integer idUniversite){
-Universite u = universiteRepository.findById(idUniversite).get();
-return  u;
+        if (optionalUniversite.isPresent()) {
+            return optionalUniversite.get();
+        } else {
+            throw new EntityNotFoundException("Universite is not found with id: " + idUniversite);
+        }
     }
+
     public  void deleteUniversite(Integer idUniversite){
         universiteRepository.delete(retrieveUniversite(idUniversite));
     }
 
-    public void assignUniversiteToDepartement(Integer idUniversite, Integer idDepartement){
-        Universite u= universiteRepository.findById(idUniversite).orElse(null);
-        Departement d= departementRepository.findById(idDepartement).orElse(null);
+    public void assignUniversiteToDepartement(Integer idUniversite, Integer idDepartement) {
+        Universite u = universiteRepository.findById(idUniversite).orElse(null);
+        Departement d = departementRepository.findById(idDepartement).orElse(null);
+
+        if (u == null) {
+            throw new EntityNotFoundException("Universite nnot found with id: " + idUniversite);
+        }
+
+        if (d == null) {
+            throw new EntityNotFoundException("Departement nottt found with id: " + idDepartement);
+        }
+
         u.getDepartements().add(d);
         universiteRepository.save(u);
     }
 
-    public Set<Departement> retrieveDepartementsByUniversite(Integer idUniversite){
-Universite u=universiteRepository.findById(idUniversite).orElse(null);
-return u.getDepartements();
+    public Set<Departement> retrieveDepartementsByUniversite(Integer idUniversite) {
+        Universite u = universiteRepository.findById(idUniversite).orElse(null);
+
+        if (u == null) {
+            throw new EntityNotFoundException("Universite nooottt found with id: " + idUniversite);
+        }
+
+        return u.getDepartements();
     }
 }
